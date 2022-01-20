c3fc3fafe8466ba72946bdceb0f935641936550e
Norbesz93 <norbesz0826@gmail.com>
2022-01-20 13:11:48 +0100

    aboutUs update

--- frontend/src/Components/AboutUs.jsx
+++ frontend/src/Components/AboutUs.jsx
@@ -1,7 +1,7 @@
 const AboutUs = ()=>{
     return(
         <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, non temporibus, deleniti error voluptas perferendis explicabo eveniet animi nemo corrupti maxime nam, assumenda numquam. Ullam consectetur reiciendis explicabo ad quis.
+            Ez egy teljesen fiktív bemutatásra készült weboldal.
         </div>
     )
 }
