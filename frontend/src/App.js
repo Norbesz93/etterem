5779e6b371d9c5ae6df814e431f11529d3dff0a2
unknown <udvardiendre@gmail.com>
2022-01-20 16:48:54 +0100

    First version of navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,20 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar';
-import Menu from './Components/Menu';
-import AboutUs from './Components/AboutUs';
-import Order from './Components/Order';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Menu from "./Components/Menu";
+import AboutUs from "./Components/AboutUs";
+import Order from "./Components/Order";
+import ContactUs from "./Components/ContactUs";
 
 function App() {
-  
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<AboutUs />}/>
-        <Route path="menu" element={<Menu />}/>
-        <Route path="order" element={<Order />}/>
+        <Route path="/" element={<AboutUs />} />
+        <Route path="menu" element={<Menu />} />
+        <Route path="order" element={<Order />} />
+        <Route path="contactUs" element={<ContactUs />} />
       </Routes>
     </div>
   );
