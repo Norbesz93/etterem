205da0537e23a935ca952b438a2bff9cf45cce6c
unknown <udvardiendre@gmail.com>
2022-05-03 16:50:48 +0200

    Mobile view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,50 +11,67 @@ import Cart from "./Components/Cart";
 import React, { useState } from "react";
 
 function App() {
-  const [ordered, setOrdered] = useState([])
-  const [cartSum, setCartSum] = useState(0)
+  const [ordered, setOrdered] = useState([]);
+  const [cartSum, setCartSum] = useState(0);
   const foodToCart = (name, spicy, meat, price) => {
-    let piece = 1
-    let foodObj = { piece: piece, name: name, price: price, spicy: spicy, meat: meat }
+    let piece = 1;
+    let foodObj = {
+      piece: piece,
+      name: name,
+      price: price,
+      spicy: spicy,
+      meat: meat,
+    };
     if (ordered.length === 0) {
-      ordered.push(foodObj)
-      setOrdered([...ordered])
+      ordered.push(foodObj);
+      setOrdered([...ordered]);
     } else {
-      let find = false
+      let find = false;
       for (const food of ordered) {
         if (name === food.name && spicy === food.spicy && meat === food.meat) {
-          find = true
-          console.log(name)
-          console.log(food.name)
-          console.log(food.piece)
-          food.piece = food.piece + 1
-          food.price = food.price + food.price
-          if (find) { break }
+          find = true;
+          console.log(name);
+          console.log(food.name);
+          console.log(food.piece);
+          food.piece = food.piece + 1;
+          food.price = food.price + food.price;
+          if (find) {
+            break;
+          }
         }
       }
       if (!find) {
-        console.log(find)
-        ordered.unshift(foodObj)
-        setOrdered([...ordered])
+        console.log(find);
+        ordered.unshift(foodObj);
+        setOrdered([...ordered]);
       }
     }
-    setCartSum(cartSum + 1)
-  }
-  const removeFromCart = (name, spicy, meat)=>{
-
-  }
+    setCartSum(cartSum + 1);
+  };
+  const removeFromCart = (name, spicy, meat) => {};
   return (
     <div className="App">
       <Navbar cartSum={cartSum} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="aboutUs" element={<AboutUs />} />
-        <Route path="menu" element={<Menu foodToCart={foodToCart} cartSum={cartSum} setCartSum={setCartSum} />} />
+        <Route
+          path="menu"
+          element={
+            <Menu
+              foodToCart={foodToCart}
+              cartSum={cartSum}
+              setCartSum={setCartSum}
+            />
+          }
+        />
         <Route path="order" element={<Order />} />
         <Route path="contactUs" element={<ContactUs />} />
-        <Route path="cart" element={<Cart ordered={ordered} removeFromCart={removeFromCart} />} />
+        <Route
+          path="cart"
+          element={<Cart ordered={ordered} removeFromCart={removeFromCart} />}
+        />
       </Routes>
-      <Footer />
     </div>
   );
 }
