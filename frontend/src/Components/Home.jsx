7939a197376660d9c1ad7496b5d148420ff4f9a3
unknown <udvardiendre@gmail.com>
2022-05-10 01:18:28 +0200

    Slideshow

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,11 +1,15 @@
 import pizzaOne from "../Media/pizza_1.jpg"
 import {Link} from "react-router-dom"
+import Slider from "../Components/Slider"
 
 
 const Home = ()=>{
     return(
         
         <div id="homeMain">
+            <div id="adSection">
+                <Slider/>
+            </div>
             <div id="homeBackground">
                 <img  id="pizzaImg" src={pizzaOne} alt="pizza"></img>
             </div>
